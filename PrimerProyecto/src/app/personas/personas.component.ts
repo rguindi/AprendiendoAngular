9840e08508ed0754953a4a44b1e10e51298833e1
rguindi <raul_fv@hotmail.com>
2024-05-20 15:58:50 +0200

    seguimos

--- PrimerProyecto/src/app/personas/personas.component.ts
+++ PrimerProyecto/src/app/personas/personas.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { PersonaComponent } from '../persona/persona.component';
 
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [PersonaComponent],
+  imports: [PersonaComponent, FormsModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.css'
 })
 export class PersonasComponent {
-  deshabilitar = true;
+  deshabilitar = false;
+  mensaje = 'No se ha agregado ninguna persona.';
+  titulo = "";
+  agregarPersona(){
+    this.mensaje = 'Persona agregada';
+  }
+
+  modificarTitulo(event: Event){
+    this.titulo = (<HTMLInputElement> event.target).value;
+  }
 
 }
+
