7c3ea1aa7a3210e0e54f70a7a1e6e8ca9f00385f
rguindi <raul_fv@hotmail.com>
2024-05-29 16:26:41 +0200

    Version compatible sierra

--- listarPersonas/src/app/app.component.ts
+++ listarPersonas/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Persona } from './persona.model';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo = 'Listado de personas';
